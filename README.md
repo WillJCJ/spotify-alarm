ed1233e8b5330a003a6b16b761fada5e0e19618a
Will Johnson <will@thedatashed.co.uk>
2020-01-30 23:38:50 +0000

    Basic Play and Pause Functionality
    - Add requrements
    - Add README.md
    - Add .gitignore
    - Add alarm module with play and pause

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spotify Alarm Clock
+
+Uses the spotify API to play a playlist.
+
+Can be scheduled to run so you can use it as an alarm clock.
---
+++ alarm/alarm.py
@@ -0,0 +1,47 @@
+import logging
+import os
+
+import fire
+import spotipy
+import spotipy.util as util
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+
+logger.setLevel(os.environ.get("LOG_LEVEL", logging.INFO))
+logger.addHandler(handler)
+
+
+scope = """
+playlist-read-private
+playlist-read-collaborative
+user-library-read
+user-modify-playback-state
+user-read-currently-playing
+user-read-playback-state
+"""
+
+
+def get_auth_token(username):
+    token = util.prompt_for_user_token(username=username, scope=scope)
+    if not token:
+        raise Exception(f"Can't get token for {username}")
+    return token
+
+
+class Controller:
+    def __init__(self, username):
+        token = get_auth_token(username)
+        self.sp = spotipy.Spotify(auth=token)
+
+    def play(self):
+        self.sp.start_playback()
+        logger.debug("playback resumed")
+
+    def pause(self):
+        self.sp.pause_playback()
+        logger.debug("playback paused")
+
+
+if __name__ == '__main__':
+    fire.Fire(Controller)
